20666484c8f443101c2c06ded6a403d1d2e1c202
Jakub Piasecki <jakubpiasecki05@gmail.com>
2024-01-19 08:43:30 +0100

    Merged roles into master

--- src/main/java/com/kvbadev/wms/data/security/PrivilegeRepository.java
+++ src/main/java/com/kvbadev/wms/data/security/PrivilegeRepository.java
@@ -1,6 +1,6 @@
-package com.kvbadev.wms.data.auth;
+package com.kvbadev.wms.data.security;
 
-import com.kvbadev.wms.models.auth.Privilege;
+import com.kvbadev.wms.models.security.Privilege;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
