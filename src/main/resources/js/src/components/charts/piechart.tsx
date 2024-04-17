a029f4b718bad52935a7ad09db767e0467f76699
Jakub Piasecki <jakubpiasecki05@gmail.com>
2024-04-17 07:43:30 +0200

    feature: added piechart to users view, updated users table to be responsive

---
+++ src/main/resources/js/src/components/charts/piechart.tsx
@@ -0,0 +1,39 @@
+import { render } from '@testing-library/react';
+import { PieChart, ResponsiveContainer, Cell, Pie, Tooltip, Legend  } from 'recharts';
+
+const data = [
+  { name: 'ROLE_ADMIN', value: 400 },
+  { name: 'ROLE_USER', value: 300 },
+  { name: 'ROLE_STAFF', value: 200 },
+];
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+export default function CustomPieChart({data}: {data: {name: string, value: number}[]}) {
+  
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <PieChart>
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          outerRadius={120}
+          fill="#8884d8"
+          dataKey="value"
+          nameKey="name"
+          label={({name}) => name}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip />
+        <Legend />
+      </PieChart>
+    </ResponsiveContainer>
+  )
+}
--- src/main/resources/js/src/components/dashboard/users/Users.tsx
+++ src/main/resources/js/src/components/dashboard/users/Users.tsx
@@ -1,37 +1,63 @@
 import Loader from "../Loader";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import { DashboardContextType } from "../Dashboard";
+import CustomPieChart from "../../charts/piechart";
+import { navbarHeight } from "../../navbar/Navbar";
+import chartData from "../../../models/chartData";
 
 export default function Users() {
-    const {users} = useOutletContext<DashboardContextType>();
+    const { users } = useOutletContext<DashboardContextType>();
     const navigate = useNavigate()
 
-    const TableData = ({value}: {value: string}) => 
-        <th className="border-[1px] p-2">{value}</th>
+    function getDistributionOfUsers(): { name: string; value: number; }[] {
+        const rolesCount: Map<string, number> = new Map();
+        users.forEach((u) => {
+            u.roleNames.forEach(r => {
+                if (rolesCount.has(r)) {
+                    rolesCount.set(r, rolesCount.get(r)! + 1)
+                } else {
+                    rolesCount.set(r, 1);
+                }
+            })
+        })
+        return Array.from(rolesCount, ([name, value]) => ({ name, value }));;
+    }
+
+    const TableData = ({ value, style }: { value: string, style?: string }) =>
+        <td className={`p-2 border-x-2 text-center ${style}`}>{value}</td>
 
     return (
-        <div className="flex w-4/5 h-full p-2 flex-col gap-4">
-            <table className="font-lato border-collapse w-full">
-                <thead>
-                    <tr>
-                        {"Id,Firstname,Lastname,Email,Roles".split(',').map(d => <TableData value={d} key={d} />)}
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map(u => {
-                        return (
-                            <tr key={u.id} className="hover:bg-gray-400 transition-all duration-75 cursor-pointer [&>*]:font-normal" onClick={() => navigate(`../users/${u.id}`)}>
-                                <TableData value={u.id?.toString() ?? ''} />
-                                <TableData value={u.firstName} />
-                                <TableData value={u.lastName} />
-                                <TableData value={u.email} />
-                                <TableData value={u.roleNames.join(',')} />
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-            {users.length === 0 && <Loader />}
+        <div className={`flex w-4/5 h-[calc(100vw-${navbarHeight})]  p-2 flex-col gap-4`}>
+            {!users.length ? <Loader /> :
+                <>
+                    <div className="overflow-auto h-1/2">
+                        <table className="font-lato border-collapse w-full h-full">
+                            <thead>
+                                <tr>
+                                    {"Id,Firstname,Lastname,Email,Roles".split(',').map(d => <TableData value={d} key={d} style="sticky top-0 z-[1] font-bold bg-white " />)}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {users.map(u => {
+                                    return (
+                                        <tr key={u.id} className="hover:bg-gray-400 transition-all duration-75 cursor-pointer [&>*]:font-normal h-2" onClick={() => navigate(`../users/${u.id}`)}>
+                                            <TableData value={u.id?.toString() ?? ''} />
+                                            <TableData value={u.firstName} />
+                                            <TableData value={u.lastName} />
+                                            <TableData value={u.email} />
+                                            <TableData value={u.roleNames.join(',')} />
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                    <span className="h-1/2 w-full flex">
+                        <CustomPieChart data={getDistributionOfUsers()} />
+                        <div className="w-full"><h1 className="text-3xl">Users activities</h1></div>
+                    </span>
+                </>
+            }
         </div>
     )
 }
